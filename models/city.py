413a1008e6c2b295c2b4fc204a0ca3ab2d1d8517
user name <user email>
2022-12-02 23:43:09 +0000

    city

--- models/city.py
+++ models/city.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+import os
+import models
 
-
-if storage_type == 'db':
-    class City(BaseModel, Base):
-        """ The city class, contains state ID and name """
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-else:
-    class City(BaseModel):
+        places = relationship('Place', backref='cities', cascade='delete')
+    else:
         state_id = ""
         name = ""
