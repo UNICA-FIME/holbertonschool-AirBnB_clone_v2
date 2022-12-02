9ddfce153208a9fba0f20a885773495465047691
user name <user email>
2022-12-02 18:20:36 +0000

    change

--- models/city.py
+++ models/city.py
@@ -5,12 +5,13 @@ from sqlalchemy import Column, String, ForeignKey
 from models import storage_type
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    if storage_type == 'db':
+if storage_type == 'db':
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    else:
+else:
+    class City(BaseModel):
         state_id = ""
         name = ""
