8b8afea52bc8d1dc6eafa04ab72e6d6845edcdbe
UNICA-FIME <juancarlosfime@gmail.com>
2022-12-02 16:40:45 +0000

    change

--- models/state.py
+++ models/state.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from models.city import City
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    if models.storage_type == "db":
+    if models.storage_type == 'db':
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade='delete')
+        cities = relationship("City", backref="state", cascade="all")
     else:
         name = ""
 
