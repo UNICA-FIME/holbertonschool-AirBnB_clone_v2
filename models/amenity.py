2e7ccb1109ee98edcaf9d5b4f8fa5233839629b5
user name <user email>
2022-12-03 01:11:00 +0000

    change

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,21 @@
-
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
+import os
+
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity: Class"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
-
-    def __init__(self, *args, **kwargs):
-        """
-            Init for inherited
-        """
-        super().__init__(*args, **kwargs)
+    """ Representation of Amenity """
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
+        name = ""
